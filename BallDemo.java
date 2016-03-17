cfa4358ba2264fe15c8f563ccf9efc2ab1fb8136
unknown <you@example.com>
2016-03-17 11:14:05 +0100

    El usuario elige el numero de bolas que se crean

--- BallDemo.java
+++ BallDemo.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.ArrayList;
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -11,6 +12,8 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
+    
+    private ArrayList<BouncingBall> bolas;
 
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
@@ -21,9 +24,9 @@ public class BallDemo
     }
 
     /**
-     * Simulate two bouncing balls
+     * Simula el número de bolas que asigna el usuario min 1.
      */
-    public void bounce()
+    public void bounce(int numBolas)
     {
         int ground = 400;   // position of the ground line
 
@@ -31,21 +34,22 @@ public class BallDemo
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
+        
+        BouncingBall a = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
 
-        // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
+        for(int i = 0; i < numBolas; i++){
+            a = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
+            a.draw();
+        }
+        a.draw();
 
         // make them bounce
         boolean finished =  false;
         while(!finished) {
             myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
+            a.move();
             // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
+            if(a.getXPosition() >= 550) {
                 finished = true;
             }
         }
